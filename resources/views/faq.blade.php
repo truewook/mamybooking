ae55e550f4d22e99c093cc258335ca04e4f6f9e5
phayak <phayak.art@gmail.com>
2017-02-08 19:03:46 +0700

    about update
    about update

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.master')
+
+@section('style')
+
+@endsection
+
+@section('content')
+<div class="page-title-container">
+    <div class="container">
+        <div class="page-title pull-left">
+            <h2 class="entry-title">About Us</h2>
+        </div>
+        <ul class="breadcrumbs pull-right">
+            <li><a href="{{url('/')}}">HOME</a></li>
+            <li class="active">About Us</li>
+        </ul>
+    </div>
+</div>
+<div class="container">
+    
+</div>
+@endsection
+
+@section('script')
+
+@endsection
