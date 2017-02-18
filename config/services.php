2dce4fc919f43a40d876fa8548299e61fae03e50
phayak <phayak.art@gmail.com>
2017-02-18 22:36:52 +0700

    11
    111

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'facebook' => [
+        'client_id' => 'your-github-app-id',
+        'client_secret' => 'your-github-app-secret',
+        'redirect' => 'http://your-callback-url',
+    ],
+
 ];
