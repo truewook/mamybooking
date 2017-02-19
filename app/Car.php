3dbaef1d8b9274a6ca721d8e7a5425fa07f2ae82
GanGenz <jquery4me@gmail.com>
2017-02-20 00:10:31 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	composer.lock

--- app/Car.php
+++ app/Car.php
@@ -4,8 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Car extends Model
 {
+
     protected $fillable = [
         'car_agency_id', 'car_generation_id', 'car_category_id','seat','door','bag_loadable','air_condition','automatic','net_rate','credit_card_fees','return_slowly','cancellation_before','active'
     ];
